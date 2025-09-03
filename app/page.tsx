42e6d43e28f2413109690491f3ac9a28b19fd519
its-robii <robiulhassanrobi33@gmail.com>
2025-09-03 18:35:22 +0600

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,49 @@
-import Image from "next/image";
-
+import { Eye } from 'lucide-react';
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+   <nav className="w-full">
+    <div className="container flex">
+      <div className="w-1/2 inset-0 h-screen bg-[url('/Left.png')] bg-cover bg-center">
+       <h2 className="font-medium text-2xl text-[#000000] pt-8 text-center">3legant.</h2>
+      </div>
+      <div className="w-1/2 h-screen overflow-hidden">
+      <form className="pt-56 pl-[88px] pb-[484px]">
+      <h1 className="font-medium text-[40px] text-[#000000] pb-6">Sign In</h1>
+      <p className="text-[#6C7275] font-normal text-[16px] pb-8">Don’t have an accout yet? <a className="text-[#38CB89] font-normal text-[16px] cursor-pointer">Sign Up</a></p>
+      <div className="mb-8">
+        <label htmlFor="username"></label>
+        <input 
+        id="username"
+        type="text"
+        placeholder="Your username or email address"
+        className="text-[#6C7275] border-b-[1px] border-b-[#E8ECEF] h-10 focus:outline-none w-full tex-[16px] font-normal cursor-pointer"
+        required
         />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+       <div className="mb-8 relative">
+        <label htmlFor="pass"></label>
+        <input 
+        id="pass"
+        type="password"
+        placeholder="Password"
+        className=" text-[#6C7275] border-b-[1px] border-b-[#E8ECEF] h-10 focus:outline-none w-full tex-[16px] font-normal cursor-pointer"
+        required
+        />
+        <button type='button'
+         className="absolute right-2 top-3 text-[#6C7275] cursor-pointer">
+          <Eye size={22}/>
+        </button>
+      </div>
+      <div className='mb-8 flex items-center justify-between'>
+      <div className='flex gap-3'>
+        <input type='checkbox'></input>
+        <p className='text-[#6C7275] tex-[16px] font-normal'>Remember me</p>
+      </div>
+      <a className='font-semibold text-[16px] text-[#141718] cursor-pointer'>Forgot password?</a>
+      </div>
+      </form>
+      </div>
     </div>
+   </nav>
   );
 }
