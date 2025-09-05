07fd8080de88f48a93989b6fe88d4c5b3474fe80
its-robii <robiulhassanrobi33@gmail.com>
2025-09-05 19:57:30 +0600

    feature section

--- components/featurer/Feature.jsx
+++ components/featurer/Feature.jsx
@@ -1,37 +1,37 @@
-"use client"
-import React from 'react'
-import ProductCard from "@/components/featurer/ProductCard"
+"use client";
+import React from "react";
+import ProductCard from "@/components/featurer/ProductCard";
 
 const Feature = () => {
   return (
-    <div className='mt-4'>
-        <div className='mx-40 flex justify-between'>
-            <h3 className='font-medium text-[40px] text-[#000000]'>Featured</h3>
-             <ul className='flex self-center gap-1'>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-             </ul>
-        </div>
-        <div>
-             <ProductCard
-        title="Shark - Men's cabretta white golf glove"
-        price="$19.00"
-        image="/Card.png"
-        rating={4}
-        badge="HOT"
-        onAddToCart={() => alert("Added to cart!")}
-      />
-      <ProductCard
-        title="Nike Running Shoes"
-        price="$99.00"
-        image="/Card.png"
-        rating={5}
-        badge="NEW"
-      />
-        </div>
+    <div className="mt-4">
+      <div className="mx-40 flex justify-between">
+        <h3 className="font-medium text-[40px] text-[#000000]">Featured</h3>
+        <ul className="flex self-center gap-1">
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+        </ul>
+      </div>
+      <div>
+        <ProductCard
+          title="Shark - Men's cabretta white golf glove"
+          price="$19.00"
+          image="/Card.png"
+          rating={4}
+          badge="HOT"
+          onAddToCart={() => alert("Added to cart!")}
+        />
+        <ProductCard
+          title="Nike Running Shoes"
+          price="$99.00"
+          image="/Card.png"
+          rating={5}
+          badge="NEW"
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Feature
+export default Feature;
